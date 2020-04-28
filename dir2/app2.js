579f3d569a837aa7d08d2b30734345332a3d9a48
wesley <1403576702@qq.com>
2020-04-28 11:56:47 +0800

    another changed

---
+++ dir2/app2.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2020-04-28 11:54:28
+ * @LastEditTime: 2020-04-28 11:55:00
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \MyGit\dir2\app2.js
+ */
+console.log("dir2")
