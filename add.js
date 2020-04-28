3e82310f9b6040e24dfa617b380a915d0f62b44d
wesley <1403576702@qq.com>
2020-04-28 10:37:20 +0800

    second commit

---
+++ add.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2020-04-28 10:13:43
+ * @LastEditTime: 2020-04-28 10:28:01
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \MyGit\add.js
+ */
+console.log('hellow world')
