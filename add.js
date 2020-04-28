415ffef5fae90068c2bd3f772ef9cb6dafb19380
wesley <1403576702@qq.com>
2020-04-28 14:40:08 +0800

    change my index

--- add.js
+++ add.js
@@ -1,10 +1,11 @@
 /*
  * @Author: your name
  * @Date: 2020-04-28 10:13:43
- * @LastEditTime: 2020-04-28 10:41:48
+ * @LastEditTime: 2020-04-28 14:38:58
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \MyGit\add.js
  */
 console.log('hellow world');
 console.log('hhh');
+console.log('我修改了js')
