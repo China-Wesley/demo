3faaf3e83b997ead20274d8e03b0cce5384cbbaf
wesley <1403576702@qq.com>
2020-04-28 11:42:01 +0800

    'changed'

--- add.js
+++ add.js
@@ -1,10 +1,10 @@
 /*
  * @Author: your name
  * @Date: 2020-04-28 10:13:43
- * @LastEditTime: 2020-04-28 10:40:51
+ * @LastEditTime: 2020-04-28 10:41:48
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \MyGit\add.js
  */
 console.log('hellow world');
-console.log('hhh')
+console.log('hhh');
